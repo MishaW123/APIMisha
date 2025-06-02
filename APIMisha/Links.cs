3defa681ee99e3a3ce3c8fd1d180d9a2b557eedb
MishaW123 <focsv002@gmail.com>
2025-06-02 15:58:53 +0300

    Update Links.cs

--- APIMisha/Links.cs
+++ APIMisha/Links.cs
@@ -1,7 +1,8 @@
-﻿namespace APIMisha
+namespace APIMisha
 {
     public class Links
     {
         public static string URI = "https://www.thesportsdb.com/api/v1/json/3/";
+        public static string Connect = "Host=hopper.proxy.rlwy.net;Port=25779;Username=postgres;Password=PlWqMHxrRweEYDuTbAJJTKHDlDZSeWxB;Database=railway;SSL Mode=Require;Trust Server Certificate=true";
     }
 }
